f3078c0fbc420caafa30ded16de96febb18ae6d7
Sagar Jolly <sagarjolly11@gmail.com>
2021-07-02 17:59:17 +0530

    v3 structure complete(broken)

---
+++ v3/Router.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app;
+
+class Router {
+    public array $getRoutes = [];
+    public array $postRoutes = [];
+
+    public function post($url, $fn) {
+        $this->getRoutes[$url] = $fn;
+    }
+
+    public function get($url, $fn) {
+        $this->postRoutes[$url] = $fn;
+    }
+
+    public function resolve() {
+        // echo '<pre>';
+        // var_dump($_SERVER);
+        // echo '</pre>';
+        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method === 'GET') {
+            $fn = $this->getRoutes[$currentUrl] ?? null;
+        } else {
+            $fn = $this->postRoutes[$currentUrl] ?? null;
+        }
+        if($fn) {
+            echo '<pre>';
+            var_dump($fn);
+            echo '</pre>';
+        } else {
+            echo 'Page not found';
+        }
+    }
+}
---
+++ v3/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9aa002609bed36514451a673e30941cff8c0d7b5',
+        'name' => 'sagar/v3',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sagar/v3' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9aa002609bed36514451a673e30941cff8c0d7b5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
